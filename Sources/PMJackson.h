9d5abcc62da06b0f2168f56ca9fa67cbbf98cfbb
Mariotaku Lee <mariotaku.lee@gmail.com>
2017-05-01 16:22:09 +0800

    updated header file

---
+++ Sources/PMJackson.h
@@ -0,0 +1,28 @@
+//
+//  PMJackson.h
+//  PMJackson
+//
+//  Created by Mariotaku Lee on 5/1/17.
+//  Copyright © 2017 Mariotaku Lee.
+//
+//  Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+//  http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+//  <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+//  option. This file may not be copied, modified, or distributed
+//  except according to those terms.
+//
+
+#if defined(__cplusplus)
+#define PMJackson_EXTERN extern "C"
+#else
+#define PMJackson_EXTERN extern
+#endif
+
+//! Project version number for JSON.
+PMJackson_EXTERN double PMJacksonVersionNumber;
+
+//! Project version string for JSON.
+PMJackson_EXTERN const unsigned char PMJacksonVersionString[];
+
+#undef PMJackson_EXTERN
+
