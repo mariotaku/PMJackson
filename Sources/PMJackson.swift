77f64d75e5897c3e73b873d1ae750649532c677b
Mariotaku Lee <mariotaku.lee@gmail.com>
2017-05-01 14:17:51 +0800

    fixed visibility

--- Sources/PMJackson.swift
+++ Sources/PMJackson.swift
@@ -1,28 +1,39 @@
 import PMJSON
 
-class JacksonAlikeParser {
+public class PMJacksonParser {
 
-    var iter: JSONParserIterator<AnyIterator<UnicodeScalar>>
+    private var iter: JSONParserIterator<AnyIterator<UnicodeScalar>>
 
     private(set) var currentEvent: JSONEvent?
 
-    var currentName: String? {
-        if (!currentIsName) {
-            return nil
-        }
-        return currentEvent?.asString()
-    }
+    /**
+     * Method that can be called to get the name associated with
+     * the current token: for field names it will
+     * be the same as what `currentEvent.asString()` returns;
+     * for field values it will be preceding field name;
+     * and for others (array values, root-level values) null.
+     */
+    public private(set) var currentName: String? = nil
 
     private var isObject: Bool = false
     private var isArray: Bool = false
     private var nextIsName: Bool = false
     private var currentIsName: Bool = false
 
-    init(_ parser: JSONParser<AnySequence<UnicodeScalar>>) {
+    public init(_ parser: JSONParser<AnySequence<UnicodeScalar>>) {
         self.iter = parser.makeIterator()
     }
 
-    @discardableResult func nextEvent() -> JSONEvent? {
+    /**
+     * Main iteration method, which will advance stream enough
+     * to determine type of the next token, if any. If none
+     * remaining (stream has no content other than possible
+     * white space before ending), null will be returned.
+     *
+     * - returns: Next token from the stream, if any found, or null
+     *   to indicate end-of-input
+     */
+    @discardableResult public func nextEvent() -> JSONEvent? {
         guard let event = iter.next() else {
             currentEvent = nil
             return nil
@@ -32,28 +43,52 @@ class JacksonAlikeParser {
             isObject = true
             nextIsName = true
             currentIsName = false
-        case .objectEnd:
-            isObject = false
-            nextIsName = false
-            currentIsName = false
+            currentName = nil
         case .arrayStart:
             isArray = true
             nextIsName = false
             currentIsName = false
-        case .arrayEnd:
+            currentName = nil
+        case .objectEnd, .arrayEnd:
             isArray = false
             nextIsName = false
             currentIsName = false
-        default: if (isObject) {
-            currentIsName = nextIsName
-            nextIsName = !nextIsName
+            currentName = nil
+        default:
+            if (isObject) {
+                currentIsName = nextIsName
+                if (currentIsName) {
+                    currentName = event.asString()
+                }
+                nextIsName = !nextIsName
+            } else {
+                currentName = nil
+                currentIsName = false
+                nextIsName = false
             }
         }
         return currentEvent
     }
 
-    func skipChildren() {
+    /**
+     * Method that will skip all child tokens of an array or
+     * object token that the parser currently points to,
+     * if stream points to
+     * `objectStart` or `arrayStart`.
+     * If not, it will do nothing.
+     * After skipping, stream will point to **matching**
+     * `objectEnd` or `arrayEnd`
+     * (possibly skipping nested pairs of OBJECT/ARRAY START/END tokens
+     * as well as value tokens).
+     * The idea is that after calling this method, application
+     * will call `nextEvent` to point to the next
+     * available token, if any.
+     */
+    @discardableResult public func skipChildren() -> PMJacksonParser {
         var depth = 0
+        if (currentEvent != .objectStart && currentEvent != .arrayStart) {
+            return self
+        }
         repeat {
             guard let event = nextEvent() else {
                 break
@@ -66,12 +101,13 @@ class JacksonAlikeParser {
             default: break
             }
         } while (depth > 0)
+        return self
     }
 }
 
-extension JSONEvent {
+public extension JSONEvent {
 
-    func asString(def: String? = nil) -> String! {
+   public func asString(_ def: String? = nil) -> String! {
         switch self {
         case .stringValue(let v):
             return v
@@ -90,7 +126,7 @@ extension JSONEvent {
         }
     }
 
-    func asInt64(def: Int64? = nil) -> Int64! {
+    public func asInt64(_ def: Int64? = nil) -> Int64! {
         switch self {
         case .stringValue(let v):
             return Int64(v) ?? def
@@ -98,6 +134,23 @@ extension JSONEvent {
             return v
         case .doubleValue(let v):
             return Int64(v)
+        case .nullValue:
+            return nil
+        case .error(let err):
+            fatalError(err.description)
+        default:
+            fatalError("Unexpected event \(eventType)")
+        }
+    }
+    
+    public func asDoublent64(_ def: Double? = nil) -> Int64! {
+        switch self {
+        case .stringValue(let v):
+            return Double(v) ?? def
+        case .int64Value(let v):
+            return Double(v)
+        case .doubleValue(let v):
+            return v
         case .nullValue:
             return nil
         case .error(let err):
